6838f3e80493dead952d09dadce5b6e2836a6871
Shailesh kumar Satapathy <shaileshsatapathy9@gmail.com>
2021-04-16 13:39:51 +0530

    start position added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,34 +4,11 @@ namespace SnakeAndLadder
 {
     class Program
     {
-        int START_POSITION = 0;
-        int NUMBER_OF_PLAYER = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake And Ladder");
-            Random random = new Random();
-            {
-                int Random = random.Next(1, 7);
-                Console.WriteLine("Number of dice " + Random);
-
-                static void checkOption()
-                {
-                    const int NOPLAY = 0;
-                    const int SNAKE = 1;
-                    const int LADDER = 2;
-                    Random rand = new Random();
-                    int play = rand.Next(1, 7);
-                    int player = 0;
-                    switch (checkOption)
-                   
-                    {
-                   
-                    }
-                }
-
-            }
-            
-            Console.ReadLine();
+            Console.WriteLine("Welcome to Snake and Ladder game");
+            gameplay gamePlay = new gameplay();
+            gamePlay.Play();
 
         }
     }
